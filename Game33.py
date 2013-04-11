d4a75e951a3175f08a1c60eac3526252903d8d5b
techy1157 <techycat1157@yahoo.com>
2013-04-11 02:10:35 -0300

    Added License

--- Game33.py
+++ Game33.py
@@ -77,7 +77,20 @@ while (True):
 	if (score>=20):
 		print ('\n\n\n\nYOU WIN!!!!!!')
 		break
+#This file is part of Python-mini-Game.
 
+#    Python-mini-Game is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    Python-mini-Game is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with Python-mini-Game.  If not, see <http://www.gnu.org/licenses/>.
 		
 	
 
